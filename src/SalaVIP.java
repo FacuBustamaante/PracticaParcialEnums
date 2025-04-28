b6ae1814dcd122bcaeaaa5e92b74faadfcde200a
FacuBustamaante <facundombustamante@gmail.com>
2025-04-28 13:40:51 -0300

    added comments for issues

--- src/SalaVIP.java
+++ src/SalaVIP.java
@@ -2,13 +2,27 @@ public class SalaVIP {
     protected String beneficios;
     private Sala sala;
 
-    public SalaVIP(String beneficios) {
+
+    /*En este punto para realizar la herencia de Sala y SalaVIP
+    * resolvi crear un objeto tipo Sala y un constructor vacio, esa solucion
+    * encontre investigando en internet pero me genera dudas de si es correcto.
+    * MISMO CASO EN CLIENTE Y CLIENTEVIP
+    * */
+
+    public SalaVIP(String beneficios, Sala sala) {
         this.beneficios = beneficios;
+        this.sala = sala;
     }
     public String getBeneficios() {
         return beneficios;
     }
     public void setBeneficios(String beneficios) {
         this.beneficios = beneficios;
+    }
+    public Sala getSala() {
+        return sala;
+    }
+    public void setSala(Sala sala) {
+        this.sala = sala;
     }
 }
